2d379fb8e5d1420b92b390d8e2e084f8a01edda6
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 02:57:46 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,37 +6,37 @@ def create_an_array
     snacks = ["Cheetos"], ["Sun Chips"], ["Lays Chips"], ["Chips Ahoy"]
 end
 
-def add_element_to_end_of_array(beverage, water)
-    beverage = ["diet coke", "pepsi", "coffee", "sparkling water"]
-    #{}"bottled water" <<
+def add_element_to_end_of_array(array, element)
+    element = "arrays!"
+      array = ["wow", "I", "am", "really", "learning"]
 end
 
-def add_element_to_start_of_array(breakfast, coffee)
-    breakfast = []
-    "coffee".unshift
+def add_element_to_start_of_array(array, wow)
+    array = ["I", "am", "really", "learning"]
+    "wow".unshift
 end
 
-def remove_element_from_end_of_array(dinner)
-    dinner = ["pizza", "spaghetti", "salad", "bread"]
+def remove_element_from_end_of_array(array)
+    array = ["I", "am", "really", "learning", "arrays!"]
     dinner[3].pop
 end
 
-def remove_element_from_start_of_array(lunch)
-    lunch = ["fries", "sandwich", "chips", "cookie"]
-    lunch.shift[4]
+def remove_element_from_start_of_array(array)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array.shift[4]
 end
 
-def retrieve_element_from_index(pizza, index_number)
-    pizza = ["sausage", "pepperoni", "olives", "cheese", "sauce"]
-    pizza[2]
+def retrieve_element_from_index(array, index_number)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    array[2]
 end
 
-def retrieve_first_element_from_array(gadgets)
-    gadgets = ["roku", "fire stick", "iphone", "laptop", "ipad"]
+def retrieve_first_element_from_array(array)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
     gadgets[0]
 end
 
-def retrieve_last_element_from_array(stores)
-    stores = ["amazon", "target", "walmart", "staples", "whole foods"]
-    stores[4]
+def retrieve_last_element_from_array(array)
+    array = ["wow", "I", "am", "really", "learning", "arrays!"]
+    stores[5]
 end
