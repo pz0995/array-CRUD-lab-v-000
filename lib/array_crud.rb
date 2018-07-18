c5b6526d6ba3fdb109884d56722b18f48c360665
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 02:24:12 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,21 +6,22 @@ def create_an_array
     snacks = ["Cheetos"], ["Sun Chips"], ["Lays Chips"], ["Chips Ahoy"]
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(beverage, water)
     beverage = []
     << "bottled water"
 end
 
-def add_element_to_start_of_array(array, element)
-
+def add_element_to_start_of_array(breakfast, coffee)
+    breakfast = []
+    coffee.push
 end
 
 def remove_element_from_end_of_array(array)
-
+    dinner = ["pizza", "spaghetti", "salad", "bread"]
 end
 
 def remove_element_from_start_of_array(array)
-
+    lunch = ["fries", "sandwich", "chips", "cookie"]
 end
 
 def retrieve_element_from_index(array, index_number)
