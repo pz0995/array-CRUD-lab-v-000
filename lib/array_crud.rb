d123f07c96c21ac63296e7339f38378bd8a70a86
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-07-18 03:02:10 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
 
       array = ["wow", "I", "am", "really", "learning"]
-    element = "arrays!".last      
+    element = "arrays!".last
 end
 
 def add_element_to_start_of_array(array, wow)
